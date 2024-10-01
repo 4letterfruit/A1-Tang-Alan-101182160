deae3696928842402485594bbef16fd0c2299e17
Alan <64178002+4letterfruit@users.noreply.github.com>
2024-09-30 21:33:37 -0400

    R-CODE
    RESP-1
    Create Adventure and Event decks with the correct number of cards

--- A1-Game/src/main/java/org/example/Deck.java
+++ A1-Game/src/main/java/org/example/Deck.java
@@ -3,13 +3,37 @@ import java.util.ArrayList;
 public class Deck {
     ArrayList<String> cardList;
 
+    // receive a list of cards in form of "Card/Count", separated by commas
+    public Deck(String cardString){
+        cardList = new ArrayList<String>();
+
+        String[] splitString = cardString.split(",");
+
+        for (String s : splitString) {
+            String[] temp = s.split("/");
+            String card = temp[0];
+            int count = Integer.parseInt(temp[1]);
+
+            for (int i = 0; i < count; i++){
+                cardList.add(card);
+            }
+        }
+    }
+
     // number of cards in the deck
     public int size(){
-        return 0;
+        return cardList.size();
     }
 
     // number of cards with specified name
     public int cardCount(String name){
-        return 0;
+        int count = 0;
+        for (String s : cardList){
+            if (s.equals(name)){
+                count++;
+            }
+        }
+
+        return count;
     }
 }
--- A1-Game/src/main/java/org/example/Game.java
+++ A1-Game/src/main/java/org/example/Game.java
@@ -3,4 +3,18 @@ package org.example;
 public class Game {
     Deck adventureDeck = null;
     Deck eventDeck = null;
+
+    // Game constructor
+    public Game(){
+        initializeDecks();
+    }
+
+    private void initializeDecks(){
+        adventureDeck = new Deck("F5/8,F10/7,F15/8,F20/7,F25/7,F30/4,F35/4,F40/2,F50/2,F70/1,D5/6,H10/12,S10/16,B15/8,L20/6,E30/2");
+        eventDeck = new Deck("Q2/3,Q3/4,Q4/3,Q5/2,E-Plague/1,E-Queen's Favor/2,E-Prosperity/2");
+    }
 }
+
+
+
+
