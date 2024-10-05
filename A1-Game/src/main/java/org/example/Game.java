d091b13c1c591b31174e781ce058b9d1ee55f8cc
Alan <64178002+4letterfruit@users.noreply.github.com>
2024-10-05 16:51:58 -0400

    R-CODE
    RESP-4
    Create players and assign them id values

--- A1-Game/src/main/java/org/example/Game.java
+++ A1-Game/src/main/java/org/example/Game.java
@@ -9,11 +9,19 @@ public class Game {
     // Game constructor
     public Game(){
         initializeDecks();
+        initializePlayers();
     }
 
     private void initializeDecks(){
         adventureDeck = new Deck("F5/8,F10/7,F15/8,F20/7,F25/7,F30/4,F35/4,F40/2,F50/2,F70/1,D5/6,H10/12,S10/16,B15/8,L20/6,E30/2");
         eventDeck = new Deck("Q2/3,Q3/4,Q4/3,Q5/2,E-Plague/1,E-Queen's Favor/2,E-Prosperity/2");
+    }
+
+    private void initializePlayers(){
+        PLAYER_1 = new Player(1);
+        PLAYER_2 = new Player(2);
+        PLAYER_3 = new Player(3);
+        PLAYER_4 = new Player(4);
     }
 }
 
--- A1-Game/src/main/java/org/example/Player.java
+++ A1-Game/src/main/java/org/example/Player.java
@@ -2,4 +2,8 @@ package org.example;
 
 public class Player {
     int id;
+
+    public Player(int id){
+        this.id = id;
+    }
 }
